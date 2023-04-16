3626984c18bd02bb99b664fbeecc3e89f469c740
kkwonju <kkwo4140@gmail.com>
2023-04-16 22:27:33 +0900

    실행환경 세팅

---
+++ src/main/java/com/kkwo/demo/KkwoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kkwo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkwoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkwoDemoApplication.class, args);
+	}
+
+}
