c0f268e07de17adc292c1eae3c15ead1e205da61
kkwonju <kkwo4140@gmail.com>
2023-04-16 23:25:57 +0900

    게시글 수정, 삭제 구현

--- src/main/java/com/kkwo/demo/controller/UsrArticleController.java
+++ src/main/java/com/kkwo/demo/controller/UsrArticleController.java
@@ -20,7 +20,7 @@ public class UsrArticleController {
 		makeTestData();
 	}
 	
-	public void makeTestData() {
+	private void makeTestData() {
 		for(int i = 1; i <= 10; i++) {
 			String title = "제목";
 			String body = "내용";
@@ -28,7 +28,7 @@ public class UsrArticleController {
 		}
 	}
 	
-	public int doWrite(String title ,String body) {
+	private int addArticle(String title ,String body) {
 		int id = lastArticleId + 1;
 		Article article = new Article(id, title, body);
 		articles.add(article);
@@ -36,16 +36,62 @@ public class UsrArticleController {
 		return id;
 	}
 	
+	private Article getArticle(int id) {
+		for(Article article : articles) {
+			if(article.getId() == id) {
+				return article;
+			}
+		}
+		return null;
+	}
+	
+	private void modifyArticle(Article article, String title, String body) {
+		article.setTitle(title);
+		article.setBody(body);
+	}
+	
+	private void deleteArticle(Article article) {
+		articles.remove(article);
+	}
+	
 	@RequestMapping("/usr/article/write")
 	@ResponseBody
-	public String write(String title, String body){
-		int id = doWrite(title, body);
+	public String doWrite(String title, String body){
+		int id = addArticle(title, body);
 		return id + "번 글이 생성됌";
+	}
+	
+	@RequestMapping("/usr/article/article")
+	@ResponseBody
+	public Article showArticle(int id){
+		return getArticle(id);
 	}
 	
 	@RequestMapping("/usr/article/list")
 	@ResponseBody
 	public List<Article> showList(){
 		return articles;
+	}
+	
+	@RequestMapping("/usr/article/modify")
+	@ResponseBody
+	public Object doModify(int id, String title, String body){
+		Article article = getArticle(id); 
+		if(article == null) {
+			return id + "번 글은 존재하지 않습니다";
+		}
+		modifyArticle(article, title, body);
+		return article;
+	}
+	
+	@RequestMapping("/usr/article/delete")
+	@ResponseBody
+	public String doDelete(int id){
+		Article article = getArticle(id); 
+		if(article == null) {
+			return id + "번 글은 존재하지 않습니다";
+		}
+		deleteArticle(article);
+		return id + "번 글을 삭제했습니다";
 	}
 }
