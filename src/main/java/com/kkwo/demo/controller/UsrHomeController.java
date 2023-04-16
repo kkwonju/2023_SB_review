cafc8ccd5cbed9ad935c2c71c6fc752617ee742d
kkwonju <kkwo4140@gmail.com>
2023-04-16 22:30:48 +0900

    count 컨트롤 액션

--- src/main/java/com/kkwo/demo/controller/UsrHomeController.java
+++ src/main/java/com/kkwo/demo/controller/UsrHomeController.java
@@ -6,9 +6,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
+	private int count = 0;
+	
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
 		return "안녕하세요";
+	}
+	
+	@RequestMapping("/usr/home/main1")
+	@ResponseBody
+	public int showMain1() {
+		count++;
+		return count;
+	}
+	
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		count = 0;
+		return "초기화";
 	}
 }
