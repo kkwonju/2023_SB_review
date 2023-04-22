94c5e9a071d575cde2256cc7a10721c77f669f96
kkwonju <kkwo4140@gmail.com>
2023-04-22 22:57:06 +0900

    list, detail에서 작성자의 닉네임 표시

--- src/main/java/com/kkwo/demo/repository/ArticleRepository.java
+++ src/main/java/com/kkwo/demo/repository/ArticleRepository.java
@@ -14,8 +14,22 @@ import com.kkwo.demo.vo.Article;
 public interface ArticleRepository {
 	public void writeArticle(int memberId, String title ,String body);
 	
+	@Select("""
+			SELECT *, M.nickname AS extra__writer
+			FROM article AS A
+			INNER JOIN `member` AS M
+			ON A.memberId = M.id
+			WHERE A.id = #{id}
+			""")
 	public Article getArticle(int id);
 	
+	@Select("""
+			SELECT *, M.nickname AS extra__writer
+			FROM article AS A
+			INNER JOIN `member` AS M
+			ON A.memberId = M.id
+			ORDER BY A.id DESC
+			""")
 	public List<Article> getArticles();
 	
 	public void modifyArticle(int id, String title, String body);
